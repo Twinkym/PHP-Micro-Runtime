f6de6e541cb8b60877351c2f8da087ee529856b3
Twinkym <twinkymen@hotmail.com>
2026-01-04 02:53:07 +0100

    feat: bootstrap inicial del runtime

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kd-tech/php-micro-runtime',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2446d8146183ecb9654e787c86d7e66bd0b02d22',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kd-tech/php-micro-runtime' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2446d8146183ecb9654e787c86d7e66bd0b02d22',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
